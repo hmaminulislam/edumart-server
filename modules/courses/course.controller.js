183e7afbc6e3c421b284c852033b1ebcd53baddc
hmaminulislam <mdaminulislamsahin14@gmail.com>
2024-01-04 22:17:18 +0600

    course filter route added

--- modules/courses/course.controller.js
+++ modules/courses/course.controller.js
@@ -20,3 +20,24 @@ exports.getCourse = async (req, res) => {
     res.send({ status: 401, error });
   }
 };
+
+// get courses with filter 
+exports.getCoursesFilter = async (req, res) => {
+
+  try {
+    const page = parseInt(req.query.page) - 1 || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const search = req.query.search || "";
+    let category = req.query.category || "All";
+
+    const categories = ["Business", "Design", "Development", "Programming"];
+    category === "All" ? (category = [...categories]) : category = req.query.category.split(',')
+
+    const courses = await courseModel.find({name: {$regex: search, $options: 'i'}}).where('category').in([...category]).skip(page * limit).limit(limit)
+    
+    res.send({page: page + 1, limit, category, search, courses})
+  }
+  catch (error) {
+    res.send({status: 401, error})
+  }
+}
--- modules/courses/course.routes.js
+++ modules/courses/course.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getCourses, getCourse } = require("./course.controller");
+const { getCourses, getCourse, getCoursesFilter } = require("./course.controller");
 const router = express.Router();
 
 // all courses get
@@ -7,5 +7,8 @@ router.get("/courses", getCourses);
 
 // single course get by id
 router.get("/course/:id", getCourse);
+
+// courses filter 
+router.get("/coursesfilter", getCoursesFilter);
 
 module.exports = router;
